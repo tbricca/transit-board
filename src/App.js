1a818b442d4e46f37faafb5c2cf0ab9dc627fbe9
tbricca <tbricca@gmail.com>
2017-11-08 18:49:44 -0800

    First commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="Container-Fluid">
+          <div className="Top">
+            <h1>Transit Board</h1>
+            <h4>Here's a board to track all the transit that is around you </h4>
+            <input className="searchbox" type='text' placeholder="Search by city..."  />
+          </div>
+          <div className="card" style="width: 20rem;">
+              <img className="card-img-top" src="..." alt="Card image cap" />
+              <div className="card-block">
+                <h4 className="card-title">Card title</h4>
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item">Cras justo odio</li>
+                <li className="list-group-item">Dapibus ac facilisis in</li>
+                <li className="list-group-item">Vestibulum at eros</li>
+              </ul>
+              <div className="card-block">
+                <a href="#" className="card-link">Card link</a>
+                <a href="#" className="card-link">Another link</a>
+              </div>
+            </div>
+        </div>
+      </div>
+    );
+  }
+}
+// value={this.state.location} onChange={this.handleLocationChange}
+export default App;
